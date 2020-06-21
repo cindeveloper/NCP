12f2ff3138e598b03bf008e39b85e6a0d1c28b56
nhutcohoi10 <66513124+nhutcohoi10@users.noreply.github.com>
2020-06-21 15:27:00 +0700

    ver0.7

---
+++ QuanLyCuaHangLinhKienPC_NCP-master/QuanLyCuaHangLinhKienPC_NCP/DAO/DataProvider.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DAO
+{
+
+
+    public class DataProvider
+    {
+        public SqlConnection cn = new SqlConnection();
+        public void Ketnoi()
+        {
+            try
+            {
+                if (cn.State == 0)
+                {
+                    cn.ConnectionString = @"Data Source=.;Initial Catalog=NCP;Integrated Security=True";
+                    cn.Open();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public void Ngatketnoi()
+        {
+            if (cn.State != 0)
+            {
+                cn.Close();
+            }
+        }
+
+        // Hiển thị Dữ Liệu DataGridview
+
+        public DataTable XemDLDGV(string sql, DataGridView dgv)
+        {
+            Ketnoi();
+
+            SqlDataAdapter adap = new SqlDataAdapter(sql, cn);
+            DataTable dt = new DataTable();
+            adap.Fill(dt);
+            dgv.DataSource = dt;
+            Ngatketnoi();
+            return dt;
+        }
+
+        public DataTable XemDL(string sql)
+        {
+            Ketnoi();
+            SqlDataAdapter adap = new SqlDataAdapter(sql, cn);
+            DataTable dt = new DataTable();
+            adap.Fill(dt);
+            Ngatketnoi();
+            return dt;
+        }
+        //Phương thức truy vấn dữ liệu: Insert, Update, Delete
+        public SqlCommand ThucThiDl(string sql)
+        {
+            Ketnoi();
+
+            SqlCommand cm = new SqlCommand(sql, cn);
+            int i = cm.ExecuteNonQuery();
+            if (i > 0)
+            {
+                MessageBox.Show("Thành công");
+            }
+            else
+            {
+                MessageBox.Show("Không thành công");
+            }
+            Ngatketnoi();
+            return cm;
+        }
+        public SqlDataReader getdata(string sql)
+        {
+            Ketnoi();
+            SqlCommand cmd = new SqlCommand(sql, cn);
+            SqlDataReader reader = cmd.ExecuteReader();
+            return reader;
+        }
+
+
+//              ███████ ]▄▄▄▄▄▄▄▄▃
+//         ▂▄▅█████████▅▄▃▂
+//        [███████████████████]
+//        ◥⊙▲⊙▲⊙▲⊙▲⊙▲⊙▲⊙▲⊙▲⊙▲⊙◤…
+
+        public int Insert(string sql)
+        {
+            Ketnoi();
+            SqlCommand cmd = new SqlCommand(sql, cn);
+            int count = cmd.ExecuteNonQuery();
+            Ngatketnoi();
+            return count;
+        }
+        public int Update(string sql)
+        {
+            Ketnoi();
+            SqlCommand cmd = new SqlCommand(sql, cn);
+            int count = cmd.ExecuteNonQuery();
+            Ngatketnoi();
+            return count;
+        }
+        public int Delete(string sql)
+        {
+            Ketnoi();
+            SqlCommand cmd = new SqlCommand(sql, cn);
+            int count = cmd.ExecuteNonQuery();
+            Ngatketnoi();
+            return count;
+        }
+        //...
+        public int GetDataScalar(string sql)
+        {
+            Ketnoi();
+            SqlCommand cmd = new SqlCommand(sql, cn);
+            object data = cmd.ExecuteScalar();
+            int count = Convert.ToInt32(data);
+            Ngatketnoi();
+            return count;
+        }
+    }
+
+
+}
---
+++ QuanLyCuaHangLinhKienPC_NCP-master/QuanLyCuaHangLinhKienPC_NCP/DTO/SanPhamDTO.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace DTO
+{
+    public class SanPhamDTO
+    {
+        public string MaSP { get; set; }
+        public string TenSP { get; set; }
+        public string LoaiSP { get; set; }
+        public decimal GiaGoc { get; set; }
+        public decimal GiaBan { get; set; }
+        public int SLT { get; set; }
+        public DateTime BaoHanh { get; set; }
+        public int KM { get; set; }
+        public string MaNCC { get; set; }
+        public int TrangThai { get; set; }
+        public string XuatXu { get; set; }
+        public DateTime NgayNhap { get; set; }
+        public SanPhamDTO()
+        {
+
+        }
+        public SanPhamDTO(string MaSanPham, string TenSanPham, string Loai, decimal Giagoc, decimal Giaban, int SL, DateTime BH, int KMai, string NCC, string XXu)
+        {
+            MaSP = MaSanPham;
+            TenSP = TenSanPham;
+            LoaiSP = Loai;
+            GiaGoc = Giagoc;
+            GiaBan = Giaban;
+            SLT = SL;
+            BaoHanh = BH;
+            KM = KMai;
+            MaNCC = NCC;
+            XuatXu = XXu;
+
+        }
+    }
+}
